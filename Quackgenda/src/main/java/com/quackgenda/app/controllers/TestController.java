d99eeb7b17a79575b7aead63b4001da41f5a2761
admin <admin@pcenterjava-11>
2018-09-10 16:51:50 +0200

    r

---
+++ Quackgenda/src/main/java/com/quackgenda/app/controllers/TestController.java
@@ -0,0 +1,23 @@
+package com.quackgenda.app.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class TestController {
+ 
+	
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+	@RequestMapping("/welcome")
+	public ModelAndView handleRequest() throws Exception {
+		logger.info("-- en welcome");		
+		ModelAndView model = new ModelAndView("testJSP");
+		model.addObject("mensajeTest", "test de prueba");
+		return model;
+	}
+	
+}
