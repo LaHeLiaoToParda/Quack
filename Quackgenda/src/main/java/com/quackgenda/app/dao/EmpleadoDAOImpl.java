6964e6b5f22531e67b31dde91d72794865d8e359
admin <admin@pcenterjava-12>
2018-09-11 09:02:52 +0200

    j

--- Quackgenda/src/main/java/com/quackgenda/app/dao/EmpleadoDAOImpl.java
+++ Quackgenda/src/main/java/com/quackgenda/app/dao/EmpleadoDAOImpl.java
@@ -1,11 +1,16 @@
 package com.quackgenda.app.dao;
 
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import com.quackgenda.app.model.Empleado;
 
 public class EmpleadoDAOImpl implements IEmpleadoDAO{
 
-	
+	@PersistenceContext	
+	private EntityManager entityManager;
 	
 	
 	public List<Empleado> listadoEmpleado() {
--- Quackgenda/src/main/java/com/quackgenda/app/service/ServiciosEmpleadoImpl.java
+++ Quackgenda/src/main/java/com/quackgenda/app/service/ServiciosEmpleadoImpl.java
@@ -4,26 +4,29 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.quackgenda.app.dao.IEmpleadoDAO;
 import com.quackgenda.app.model.Empleado;
 
 @Service
 @Transactional
 public class ServiciosEmpleadoImpl implements IServiciosEmpleado{
 	
-	private IServiciosEmpleado datos = new ServiciosEmpleadoImpl();
+	@Autowired
+	private IEmpleadoDAO userDAO;
 
 	
 	public List<Empleado> listadoEmpleado() {
 		
-		return datos.listadoEmpleado();
+		return userDAO.listadoEmpleado();
 	}
 
 	
 	public Empleado fichaTecnica(int idEmpleados) {
 		
-		return datos.fichaTecnica(idEmpleados);
+		return userDAO.fichaTecnica(idEmpleados);
 	}
 	
 	
