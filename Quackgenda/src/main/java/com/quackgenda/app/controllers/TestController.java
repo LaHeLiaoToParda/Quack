05910bfb62be3790ca898ada2a1b2e8921ccde6d
admin <admin@tplwkpcente-05>
2018-09-11 09:55:31 +0200

    Controller

--- Quackgenda/src/main/java/com/quackgenda/app/controllers/TestController.java
+++ Quackgenda/src/main/java/com/quackgenda/app/controllers/TestController.java
@@ -1,31 +1,41 @@
 package com.quackgenda.app.controllers;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.quackgenda.app.model.Empleado;
+import com.quackgenda.app.services.IServiciosEmpleado;
  
 @Controller
 public class TestController {
  
+	@Autowired
+	private IServiciosEmpleado serviciosEmpleado;
 	
 	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
 	@RequestMapping("/welcome")
 	public ModelAndView handleRequest() throws Exception {
-		logger.info("-- en welcome");		
-		ModelAndView model = new ModelAndView("testJSP");
-		model.addObject("mensajeTest", "test de prueba");
+		logger.info("-- Pagina Principal");
+		List<Empleado>listEmpleados = serviciosEmpleado.listadoEmpleado();
+		ModelAndView model = new ModelAndView("welcome");
+		model.addObject("listEmpleados", listEmpleados);
 		return model;
 	}
 	
-	@RequestMapping("/welcome")
+	/*@RequestMapping("/welcome")
 	public ModelAndView loadTest() throws Exception {
 		logger.info("-- en /welcome");		
 		ModelAndView model = new ModelAndView("welcome");
 //		model.addObject("mensajeTest", "test de prueba");
 		return model;
 	}
+	*/
 	
 }
